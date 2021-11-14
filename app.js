a5b876158401a4df68cdaf2af8782b37e2e14585
Axoncodes <alireza.ataee01@gmail.com>
2021-11-14 04:47:44 -0500

    updating routes based on new xmlfile changes

--- app.js
+++ app.js
@@ -2,12 +2,10 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const multer  = require('multer')
-// const server = require('server');
 const cors = require('cors')
 app.use(cors())
 const upload = multer({ dest: '../../uploads/' })
-// const { get, post } = server.router;
-const axConvertor = require('./src/xmltojson');
+const xmlToJson = require('./lib/xmltojson');
 const root = '/xmltojson';
 const PORT = process.env.PORT || 8000;
 
@@ -21,14 +19,12 @@ app.get(`${root}/`, (req, res) => {
 });
 
 app.get(`${root}/web`, (req, res) => {
-  res.sendFile(path.join(__dirname, './src/xmltojson.js'));
+  res.sendFile(path.join(__dirname, './lib/xmltojson.js'));
 });
 
 app.post(`${root}/convert`, upload.single('file'), async (req, res) => {
-  if(req.file) {
-    const converted = await axConvertor(path.join(__dirname, `./${req.file.path}`), true, false);
-    res.send(converted)
-  }else res.send(await axConvertor(req.body.code, false, false))
+  if(req.file) res.send(await xmlToJson.axConvertor(path.join(__dirname, `./${req.file.path}`), true, false))
+  else res.send(await xmlToJson.axConvertor(req.body.code, false, false))
 })
 
 
