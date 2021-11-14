323658abce66c11a0ada387a6671bb928bb4e75f
Axoncodes <alireza.ataee01@gmail.com>
2021-11-14 02:20:10 -0500

    fix the direct code convertor

--- app.js
+++ app.js
@@ -2,16 +2,22 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const multer  = require('multer')
+// const server = require('server');
 const cors = require('cors')
 app.use(cors())
 const upload = multer({ dest: '../uploads' })
+// const { get, post } = server.router;
 const axConvertor = require('./src/xmltojson');
 const root = '/xmltojson';
 const PORT = process.env.PORT || 8000;
 
+app.use((req, res, next) => {
+  console.log(`${req.method} : ${req._parsedUrl.path}`);
+  next();
+})
 
 app.get(`${root}/`, (req, res) => {
-  res.sendFile(path.join(__dirname, './views/index.html'));
+  res.sendFile(path.join(__dirname, './index.html'));
 });
 
 app.get(`${root}/web`, (req, res) => {
@@ -23,7 +29,7 @@ app.post(`${root}/convert`, upload.single('file'), async (req, res) => {
     const converted = await axConvertor(path.join(__dirname, `./${req.file.path}`), true, false);
     res.send(converted)
   }
-  res.send(await convert(req.body.code, false))
+  res.send(await axConvertor(req.body.code, false, false))
 })
 
 
