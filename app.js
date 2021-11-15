e8f50a51d2bcc4e5a9a7a27f555b5601a6061b8c
Axoncodes <alireza.ataee01@gmail.com>
2021-11-15 09:18:15 -0500

    checkpoint

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const multer  = require('multer')
 const cors = require('cors')
 app.use(cors())
 const upload = multer({ dest: '../../../uploads/' })
-const xmlAndJson = require('./lib/xmlandjson');
+const xmlAndJson = require('./lib');
 const root = '/xmlandjson';
 const PORT = process.env.PORT || 8001;
 
@@ -15,22 +15,25 @@ app.use((req, res, next) => {
 })
 
 app.get(`${root}/`, (req, res) => {
+  console.log(typeof module);
+  console.log(typeof define);
   res.sendFile(path.join(__dirname, './index.html'));
 });
 
 app.get(`${root}/web`, (req, res) => {
-  res.sendFile(path.join(__dirname, './lib/xmlandjson.min.js'));
+  res.sendFile(path.join(__dirname, './lib/index.js'));
 });
 
 app.post(`${root}/convert`, upload.single('file'), async (req, res) => {
   if(req.file) {
-    const response = await xmlAndJson.xmltojson(path.join(__dirname, `./${req.file.path}`), true, false);
+    const response = await xmlAndJson.convert(path.join(__dirname, `./${req.file.path}`), true, false);
     res.send(response)
   } else {
-    const response = await xmlAndJson.xmltojson(req.body.code, false, false);
+    const response = await xmlAndJson.convert(req.body.code, false, false);
     res.send(response)
   }
 })
+
 
 
 app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
