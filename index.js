3f8a5c24908093254e9030cc14f5b94eb51ac6ec
aXlireza <alireza.ataee01@gmail.com>
2022-01-11 16:14:22 +0330

    Optimization

--- index.js
+++ index.js
@@ -3,137 +3,68 @@
  * @author Axoncodes
  */
 
-const jsontosvg = require('./lib/jsontosvg')
-const svgtojson = require('./lib/svgtojson')
-const svgicontosvgfont = require('./lib/svgicontosvgfont')
-const convertToPath = require('./lib/convertToPath')
-const fs = require('fs');
+const parseSvg = require('./lib/parser/parseSvg')
+const parseJson = require('./lib/parser/parseJson')
+const parseSvgfont = require('./lib/parser/parseSvgfont')
+const parsePath = require('./lib/parser/parsePath')
+const parseFormat = require('./lib/parser/parseFormat')
+const parse = require('./lib/helpers/parse')
+const fileHelper = require('./lib/helpers/file')
 
 /**
  * @param {string/file} input : the path to file or string as data
  * @param {boolean} outputFormat: the version of outputed data : svg, json, fontSVG
  * @param {string} filename: the name of file the data should be written on
- * @param {boolean} unify: Whether the SVG specific tags should be all converted to PATH or not
+ * @param {boolean} unifySvg: Whether the SVG specific tags should be all converted to PATH or not
  */
-async function convert({input, outputFormat, filename, unify = false} = {}) {
-  // check if it is a file or string of data (true:file, false:string)
-  const file = fileOrString(input);
-  // readt the data of it is file
-  const data = await getData(input, file);
-  // check if it's a json or svg (true:svg, false:json)
-  const format = isitJson(data);
-  // Transform the data
-  var mainData = format
-    ? JSON.stringify(svgtojson(xmlValidation(data)))
-    : jsontosvg(JSON.parse(jsonValidation(data)));
-  // activate the convertToPath process if it's 'fontsvg'
-  if(outputFormat.toLowerCase() == 'fontsvg') unify = true;
-  // execute the convertToPath process
-  mainData = unify ? convertToPath(mainData) : mainData;
+function convert(initOptions) {
+  const options = prepare(initOptions);
+  // check if the data is a file path, then read the file, otherwise, return the string of data(input)
+  return fileHelper.getData(options)
+  // execute the parsePath process
+  .then(data => parsePath(data, options))
   // Transform the result again to the custom format if requested
-  mainData = outputFormat ? convertBack(outputFormat, mainData) : mainData
+  .then(data => assign(data, options))
   // store result
-  return createfile(mainData, filename);
+  .then(data => fileHelper.createfile(data, options))
 }
 
-function fileOrString(data) {
-  const regExp = /[^n{a-z,A-Z,0-9},.,/,:,\\,_,-,  , -]/;
-  return data.match(regExp) ? false : true
+function prepare(options) {
+  // default
+  if(!options.unifySvg) options.unifySvg = false;
+  // default
+  if(!options.outputFormat) options.outputFormat = 'json';
+  // activate the parsePath process if it's 'fontsvg'
+  if(options.outputFormat.toLowerCase() == 'fontsvg') options.unifySvg = true;
+
+  return options;
 }
 
-function createfile(data, filename) {
-  return !filename
-    ? data
-    : new Promise((resolve) => {
-        fs.writeFile(filename, data, function (err) {
-          if (err) throw err;
-          resolve('Saved!')
-        });
-      });
-} 
+function parseAbsolute(initOptions) {
+  const options = prepare(initOptions);
 
-function getData(data, file) {
-  return file ? readFile(data) : data;
+  return fileHelper.getData(options)
+  .then(parseJson)
+  .then(data => data.map(tag => parse.parseContour(tag)))
+  .then(path => parseFormat.parseFormat(path, 'absolute'))
+  .then(data => assign(data, options))
+  .then(data => fileHelper.createfile(data, options))
 }
 
-function isitJson(data) {
-  return data.indexOf('<') == 0
-}
-
-function convertBack(outputFormat, data) {
+function assign(data, {outputFormat}) {
+  // skip if there is no outputFormat set
+  if(!outputFormat) return data;
   
-  if(outputFormat.toLowerCase() == 'svg')
-    return data.indexOf('<')==0
-      ? data
-      : jsontosvg(JSON.parse(jsonValidation(data)));
-
-  if(outputFormat.toLowerCase() == 'json')
-    return data.indexOf('<')==0
-      ? JSON.stringify(svgtojson(xmlValidation(data)))
-      : data;
-
-  if(outputFormat.toLowerCase() == 'fontsvg')
-    return svgicontosvgfont(
-      data.indexOf('<')==0
-        ? svgtojson(xmlValidation(data))
-        : jsonValidation(data)
-      );
-
-  throw 'Invalid outputFormat'
-
-}
-
-function xmlValidation(data) {
-  if(!data || data.length==0) throw 'No DATA send';
-  const repeates = repeatesF(data);
-  if(repeates['>'] != repeates['<']) throw "tags opening and closings don't match";
-  if(data.indexOf('<')==-1 || data.indexOf('>')==-1) throw 'no valid tag found';
-  return data;
-}
-function jsonValidation(data) {
-  if(!data || data.length==0) throw 'No DATA send';
-  if(data.indexOf('{')==-1 || data.indexOf('}')==-1) throw 'no valid object found';
-  const repeates = repeatesF(data);
-  if(repeates['['] != repeates[']']) throw "[] opening and closings don't match";
-  if(repeates['{'] != repeates['}']) throw "{} opening and closings don't match";
-  return data;
-}
-
-// function webFileReader(file) {
-//   if(!file || file.length==0) throw 'File Invalid';
-//   return new Promise((resolve) => {
-//     const reader = new FileReader()
-//     reader.onloadend = () => resolve(reader.result)
-//     reader.readAsText(file)
-//   })
-// }
-
-function readFile(filepath, removeAfter=false) {
-  if(!filepath || filepath.length==0) throw 'File Invalid';
-  return new Promise((resolve, reject) => {
-    fs.readFile(filepath, 'utf8', async (error, fileContent) => {
-    if (error != null) throw error
-    if(removeAfter) await removeFile(filepath)
-    resolve(fileContent);
-    });
-  });
-}
-function removeFile(filepath) {
-  if(!filepath || filepath.length==0) throw 'removeFile error';
-  return new Promise((resolve, reject) => {
-    fs.unlink(filepath, (error) => {
-      if (error != null) throw error;
-      resolve(true);
-    });
-  });
-}
-function repeatesF(data) {
-  var obj={}
-  for(x = 0, length = data.length; x < length; x++) {
-      var l = data.charAt(x)
-      obj[l] = (isNaN(obj[l]) ? 1 : obj[l] + 1);
+  switch(outputFormat.toLowerCase()) {
+    case 'svg': return parseSvg(data)
+    case 'json': return parseJson(data)
+    case 'fontsvg': return parseSvgfont(data)
+    default: return data;
   }
-  return obj
 }
 
-module.exports = convert;
+module.exports = {
+  convert,
+  parsePoints: parse.parseContour,
+  parseAbsolute,
+};
